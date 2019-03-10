b88f091cc0024f8d7bea5a72940065b211f839dd
Ihor Tretiak <igor.tretyak358@gmail.com>
2019-03-10 10:46:54 +0200

    Added converter model

--- backend/converter/models.py
+++ backend/converter/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from .converter import Converter
+
+class NumberConverter(models.Model):
+    roman_number = models.CharField(max_length=255)
+    int_number = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        converter = Converter()
+        if not self.roman_number:
+            self.int_number = converter.roman_to_int(self.roman_number)
+        else:
+            self.roman_number = converter.int_to_roman(self.int_number)
+        super(NumberConverter, self).save(*args, **kwargs)
